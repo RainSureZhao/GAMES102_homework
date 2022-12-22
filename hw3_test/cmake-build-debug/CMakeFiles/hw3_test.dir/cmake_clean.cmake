1844c80e8fde48a44dd46d1118c08e4ab5a3ad01
RainSure <1114560341@qq.com>
2022-12-22 13:41:22 +0800

    add hw3_test

---
+++ hw3_test/cmake-build-debug/CMakeFiles/hw3_test.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw3_test.dir/MathUtil.cpp.obj"
+  "CMakeFiles/hw3_test.dir/imgui/imgui.cpp.obj"
+  "CMakeFiles/hw3_test.dir/imgui/imgui_demo.cpp.obj"
+  "CMakeFiles/hw3_test.dir/imgui/imgui_draw.cpp.obj"
+  "CMakeFiles/hw3_test.dir/imgui/imgui_impl_glfw.cpp.obj"
+  "CMakeFiles/hw3_test.dir/imgui/imgui_impl_opengl3.cpp.obj"
+  "CMakeFiles/hw3_test.dir/imgui/imgui_widgets.cpp.obj"
+  "CMakeFiles/hw3_test.dir/main.cpp.obj"
+  "CMakeFiles/hw3_test.dir/src/glad.c.obj"
+  "hw3_test.exe"
+  "hw3_test.exe.manifest"
+  "hw3_test.pdb"
+  "libhw3_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/hw3_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
